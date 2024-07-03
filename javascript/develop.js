fafba420d5eb515bf4da86f95140e8a493497c78
Marcos_ <delaosacruzmarcos@users.noreply.github.com>
2024-07-03 07:55:41 -0400

    adding side bar with basic animation

--- javascript/develop.js
+++ javascript/develop.js
@@ -11,3 +11,33 @@ function greet() {
             hello world
         </h3>`;
 }
+
+
+ /**************** SIDE BAR ANNIMATIONS *********************/
+
+function openLeftSideBar() {
+    var mainContent = document.getElementById("content");
+    var sideBar = document.getElementById("leftSideNav");
+
+    // Increase width of sidebar
+    mainContent.style.marginLeft = "150px";
+    sideBar.style.width = "150px";
+
+    // Unfocus main content
+    mainContent.classList.add("overlayed");
+    
+}
+
+function closeLeftSideBar() {
+    var mainContent = document.getElementById("content");
+    var sideBar = document.getElementById("leftSideNav");
+
+    // Decrease width of sidebar
+    mainContent.style.marginLeft = "40px";
+    sideBar.style.width = "40px";
+
+    // Focus main content
+    if (mainContent.classList.contains("overlayed")){
+        mainContent.classList.remove("overlayed");
+    }
+}
