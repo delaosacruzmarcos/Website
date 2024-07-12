b6c5d0ec3283109b1605e7d45c8b21702f829b30
Marcos_ <delaosacruzmarcos@users.noreply.github.com>
2024-07-12 18:14:46 -0400

    Sidebar content updates in conjunction with changing of highlighted icon.

--- javascript/develop.js
+++ javascript/develop.js
@@ -44,13 +44,25 @@ function closeLeftSideBar() {
 function toggleButton(index) {
     var toggle = true;
     var element = document.getElementsByClassName("icon-current")[0];
+    var icon = document.getElementById(chooseIcon(index));
     if (!(element === undefined)){
+
+            // Turn off Icon highlighting
             element.classList.remove("icon-current");
             element.classList.add("icon");
+
+            const iconList = ["fileText", "searchText", "versionText", "debugText", "extensionText"];
+            // Toggle all text off
+            for (text in iconList){
+                var elem = document.getElementById(iconList[text]);
+                if (elem.classList.contains("icon-element-visible")){
+                    elem.classList.remove("icon-element-visible");
+                    elem.classList.add("icon-element-invisible");
+                }
+            }
             if (element.id == index){
                 toggle = false;
                 closeLeftSideBar();
-                chooseIcon(index);
             }
     }
     if (toggle){
@@ -58,18 +70,14 @@ function toggleButton(index) {
         var iconIndex = document.getElementById(index);
         iconIndex.classList.remove("icon");
         iconIndex.classList.add("icon-current");
+        icon.classList.add("icon-element-visible");
+        icon.classList.remove("icon-element-invisible");
     }
 }
 
 // Using the index toggle the Icon related to that index on
 function chooseIcon(index){
-    alert(index);
-    //Convert index from container to Icon
-    var idx = str.indexOf('Icon');
-    alert(idx);
-    str = str.substring(0, idx);
-    alert(str)
-    str = str + "Text";
-    alert(str);
-
+    var idx = index.indexOf('Icon');
+    str = index.substring(0, idx);
+    return str + "Text";
 }
